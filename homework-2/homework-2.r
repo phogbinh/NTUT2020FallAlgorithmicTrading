57e67ea27688cfde37e76d7597d05f197e18bb07
bill <phongbinh2511@gmail.com>
2020-10-16 16:40:42 +0800

    [master] Fixed commission fee to be 0.1425%.

--- homework-2/homework-2.r
+++ homework-2/homework-2.r
@@ -16,10 +16,10 @@ stock <- to.monthly(stock)
 cash <- -Op(stock) + Cl(stock)
 plot(cumsum(cash), main='Cash without commission fee')
 
-# Take into account commission fee being 0.1425.
+# Take into account commission fee being 0.1425%.
 # Commission fee is rebated by 50%.
 # Seller must also pay 0.3% government tax.
-buy_in <- Op(stock) * (1 + 0.1425 * 0.5)
-sell_out <- Cl(stock) * (1 - 0.1425 * 0.5 - 0.003)
+buy_in <- Op(stock) * (1 + 0.001425 * 0.5)
+sell_out <- Cl(stock) * (1 - 0.001425 * 0.5 - 0.003)
 cash <- sell_out - buy_in
 plot(cumsum(cash), main='Cash with commission fee')
