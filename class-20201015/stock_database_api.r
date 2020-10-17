e19a60a50c904f44cd87b78a3e21ee11c5500b41
bill <phongbinh2511@gmail.com>
2020-10-17 10:02:26 +0800

    [master] Hid away the API token and corrected assignment operations.

--- class-20201015/stock_database_api.r
+++ class-20201015/stock_database_api.r
@@ -1,15 +1,15 @@
+# This code can only be run when you are connected to local network of National University of Technology (NTUT).
 library(jsonlite)
 library(quantmod)
 
-api_url = "http://140.124.93.179:5888/v1/history/stock?token=73da7bb9d2a475bbc2ab79da7d4e94940cb9f9d5&symbol_id=2337&data_column=STK.date,STK.o,STK.h,STK.l,STK.c,STK.v&start_date=2018-05-01&end_date=2018-05-20"
-
-api = fromJSON(api_url)
-
-api = as.data.frame(api)
-names(api) <- c("date","open","high","low","close","volume")
+api_token <- Sys.getenv('iLoveTradingLabStockDatabaseApiToken')
+api_url <- paste('http://140.124.93.179:5888/v1/history/stock?token=', api_token, '&symbol_id=2337&data_column=STK.date,STK.o,STK.h,STK.l,STK.c,STK.v&start_date=2018-05-01&end_date=2018-05-20',
+                 sep='')
+api <- fromJSON(api_url)
+api <- as.data.frame(api)
+names(api) <- c('date', 'open', 'high', 'low', 'close', 'volume')
 
 result <- api[-1]
 row.names(result) <- api$date
-
 
 result
