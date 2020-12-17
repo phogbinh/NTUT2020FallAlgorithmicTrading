57a1c4caa692ae3d69b9a917e28159e4eee08fa0
bill <phongbinh2511@gmail.com>
2020-12-17 16:12:46 +0800

    [master] Added for loop body indent.

--- ifm.r
+++ ifm.r
@@ -8,29 +8,29 @@ rm(list=ls())
 # Retrieve stock data.
 stocks = c('2330', '2454', '2317', '2308', '2303', '1301', '2412', '1303', '2891', '2882', '3008', '2881')
 for (stock_id in stocks) {
-start_date <- ''
-end_date <- Sys.Date()
-api_token <- Sys.getenv('iLoveTradingLabStockDatabaseApiToken')
-api_url <- paste('http://140.124.93.179:5888/v1/history/stock?token=',
-                 api_token,
-                 '&symbol_id=',
-                 stock_id,
-                 '&data_column=STK.date,STK.o,STK.h,STK.l,STK.c,STK.v&start_date=',
-                 start_date,
-                 '&end_date=',
-                 end_date,
-                 sep='')
-stock <- as.data.frame( fromJSON(api_url) )
-time_vector <- as.POSIXct( strptime( stock[, 1], "%Y-%m-%d",
-                                     tz=Sys.timezone() ) )
-stock <- xts( cbind( as.numeric( stock[, 2] ),
-                     as.numeric( stock[, 3] ),
-                     as.numeric( stock[, 4] ),
-                     as.numeric( stock[, 5] ),
-                     as.numeric( stock[, 6] ) ),
-              time_vector )
-colnames(stock) <- c('open', 'high', 'low', 'close', 'volume')
-saveRDS(stock, stock_id)
+    start_date <- ''
+    end_date <- Sys.Date()
+    api_token <- Sys.getenv('iLoveTradingLabStockDatabaseApiToken')
+    api_url <- paste('http://140.124.93.179:5888/v1/history/stock?token=',
+                     api_token,
+                     '&symbol_id=',
+                     stock_id,
+                     '&data_column=STK.date,STK.o,STK.h,STK.l,STK.c,STK.v&start_date=',
+                     start_date,
+                     '&end_date=',
+                     end_date,
+                     sep='')
+    stock <- as.data.frame( fromJSON(api_url) )
+    time_vector <- as.POSIXct( strptime( stock[, 1], "%Y-%m-%d",
+                                         tz=Sys.timezone() ) )
+    stock <- xts( cbind( as.numeric( stock[, 2] ),
+                         as.numeric( stock[, 3] ),
+                         as.numeric( stock[, 4] ),
+                         as.numeric( stock[, 5] ),
+                         as.numeric( stock[, 6] ) ),
+                  time_vector )
+    colnames(stock) <- c('open', 'high', 'low', 'close', 'volume')
+    saveRDS(stock, stock_id)
 }
 
 # Strategy.
